261f89e8302db62263553211f779327759a1e9ff
Aziz Moe <aziznageeye9@gmail.com>
2021-12-24 22:02:55 +0300

    jsdoc tryout

---
+++ azuz/main.js
@@ -0,0 +1,14 @@
+/**
+ * This code squares any number in you
+ * put as an argument
+ * but i wrote a default number "x=2" 
+ * in the parameter
+ * 
+ * @param {number} -x is a number
+ * @returns {string}
+ */
+ let square = function(x=2) {
+    return x*x;
+    }
+    console.log(`This is a squared number ${square()}`);
+    
